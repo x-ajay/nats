7dbcb8af49cbe3166fc73be72da53d93b5bd2067
Ajay Kumbhar <ajayakumbhar1234@gmail.com>
2024-05-31 12:43:28 +0530

    req/reply communication

--- example/pub-sub/publisher/main.go
+++ example/pub-sub/publisher/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/nats-io/nats.go"
 )
+
+type Payload struct {
+	Data  string `json:"data,omitempty"`
+	Count int    `json:"count,omitempty"`
+}
 
 func main() {
 	nc, err := nats.Connect(nats.DefaultURL)
@@ -15,14 +21,23 @@ func main() {
 	}
 	defer nc.Close()
 
+	count := 0
+	timeout := time.Millisecond * 500
+
+	payload := &Payload{
+		Data: "hello world",
+	}
 	for {
-		msg := "Hello World!"
-		err = nc.Publish("intros", []byte(msg))
-		if err != nil {
-			fmt.Println("[error] message failed to published", err)
-		}
-		log.Println("[info] message published", msg)
+		payload.Count = count
+		msg, _ := json.Marshal(payload)
+		reply, err := nc.Request("intros", msg, timeout)
 		time.Sleep(1 * time.Second)
+		if err != nil {
+			fmt.Println("[error] failed to send message=", payload.Data, "count=", payload.Count, "err=", err)
+			continue
+		}
+		count++
+		log.Println("[info] message=", payload.Data, "count=", payload.Count, "reply=", string(reply.Data))
 	}
 
 }
--- example/pub-sub/subscriber/main.go
+++ example/pub-sub/subscriber/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/nats-io/nats.go"
 )
+
+type Payload struct {
+	Data  string `json:"data,omitempty"`
+	Count int    `json:"count,omitempty"`
+}
 
 func main() {
 
@@ -17,7 +23,12 @@ func main() {
 	defer nc.Close()
 
 	nc.Subscribe("intros", func(msg *nats.Msg) {
+		payload := &Payload{}
+		_ = json.Unmarshal(msg.Data, payload)
+		replyMsg := fmt.Sprintf("received payload %d", payload.Count)
+		msg.Respond([]byte(replyMsg))
 		fmt.Println("[info] received message: ", string(msg.Data))
+
 	})
 
 	time.Sleep(1 * time.Minute)
